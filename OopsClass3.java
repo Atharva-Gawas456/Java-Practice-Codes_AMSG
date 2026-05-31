66bfc89c0f9267f62f8000c3ac053bc8f0afa40c
Atharva-Gawas456 <atharva.gawas182@gmail.com>
2026-05-31 16:30:47 +0530

    Oops Practice Example 3

---
+++ OopsClass3.java
@@ -0,0 +1,16 @@
+class Dog{
+    String breed = "Dalmatian";
+    int age = 6;
+    String color = "Black and White";
+}
+
+public class OopsClass3 {
+    public static void main(String[] args) {
+        Dog dog = new Dog();
+
+        System.out.println(dog.breed);
+        System.out.println(dog.age);
+        System.out.println(dog.color);
+
+    }
+}
